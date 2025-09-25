bf3cc821fb8c148e9ac5dfb39f280884195c2191
KaluleEnochAteesa <kal24017@byui.edu>
2025-09-25 23:53:06 +0500

    error handling

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require('./routes/inventoryRoute');
+const utilities = require("./utilities"); // Add this line to bring utilities into scope
 
 /* ***********************
  * View Engine and Templates
@@ -18,14 +20,37 @@ const static = require("./routes/static")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 //Index route
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" })
+app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
+
 
 /* ***********************
  * Local Server Information
